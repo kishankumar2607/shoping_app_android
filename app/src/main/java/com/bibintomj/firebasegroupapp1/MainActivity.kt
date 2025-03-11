a668acd19dee68c3054e82d054d0d8a0006946df
Bibin Joseph <bibintomj@gmail.com>
2025-03-11 08:09:57 -0400

    Changed flow

--- app/src/main/java/com/bibintomj/firebasegroupapp1/MainActivity.kt
+++ app/src/main/java/com/bibintomj/firebasegroupapp1/MainActivity.kt
@@ -2,27 +2,13 @@ package com.bibintomj.firebasegroupapp1
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.bibintomj.firebasegroupapp1.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,8 +24,8 @@ class MainActivity : AppCompatActivity() {
 
         val btnGetStarted: Button = findViewById(R.id.btnGetStarted)
         btnGetStarted.setOnClickListener({
-//            createSigninIntent()
-            navigateToProductListing()
+            createSigninIntent()
+//            navigateToProductListing()
         })
     }
 
